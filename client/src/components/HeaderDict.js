dc963577d6d53c63a19a6a67f749ef5291df02e5
Berlin <Berline_lucien@yahoo.com>
2022-11-06 01:38:50 -0500

    dictionary api

---
+++ client/src/components/HeaderDict.js
@@ -0,0 +1,60 @@
+import { createMuiTheme, TextField, ThemeProvider } from "@material-ui/core";
+import React from "react";
+import MenuItem from "@material-ui/core/MenuItem/MenuItem";
+import { debounce } from "lodash";
+
+const HeaderDict = ({
+  category,
+  setCategory,
+  setWord,
+  word,
+  setMeanings,
+  LightTheme,
+}) => {
+  const darkTheme = createMuiTheme({
+    palette: {
+      primary: {
+        main: LightTheme ? "#000" : "#fff",
+      },
+      type: LightTheme ? "light" : "dark",
+    },
+  });
+
+    const handleChange = (e) => {
+    setCategory(e.target.value);
+    setWord("");
+    setMeanings([]);
+  };
+
+    const handleText = debounce((text) => {
+    setWord(text);
+  }, 500);
+
+  return (
+    <div className="header">
+      <span className="title">{word ? word : "Word Hunt"}</span>
+      <div className="inputs">
+        <ThemeProvider theme={darkTheme}>
+          <TextField
+            className="search"
+            id="filled-basic"
+            // value={word}
+            label="Search a Word"
+            onChange={(e) => handleText(e.target.value)}
+          />
+          <TextField
+            select
+            label="Language"
+            value={category}
+            onChange={(e) => handleChange(e)}
+            className="select"
+          >
+           
+          </TextField>
+        </ThemeProvider>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderDict;
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,6 +2,9 @@ import React from 'react'
 import NavBar from '../components/NavBar/NavBar';
 import HeroHeader from '../components/HeroHeader/HeroHeader';
 import ReferenceLayout from '../components/ReferenceCard/ReferenceLayout';
+import DictionaryAPI from "./DictionaryAPI";
+
+
 const Landing = () => {
   return (
       <div>
@@ -10,7 +13,9 @@ const Landing = () => {
               header="Reference tool for students"
               summary="Addressing the lack of reference photos of dermatological conditions on skin of color. We help nursing and medical students be able to identify how skin conditions look on black/brown skin."
           />
-         <ReferenceLayout />
+          <DictionaryAPI/>
+          <ReferenceLayout />
+          
       </div>
   )
 }
