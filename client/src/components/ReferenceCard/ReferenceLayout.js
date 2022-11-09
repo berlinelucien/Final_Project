891acfc1db8f98a9122876c04254ad8ab49dc3f9
Berlin <Berline_lucien@yahoo.com>
2022-11-09 17:43:14 -0500

    search feature complete, fixed card componenet, added box from material ui to keep shape together

--- client/src/components/ReferenceCard/ReferenceLayout.js
+++ client/src/components/ReferenceCard/ReferenceLayout.js
@@ -2,11 +2,27 @@ import React, { useEffect, useState} from "react";
 import CardInfo from "../CardInfo";
 import Grid from '@mui/material/Grid';
 import "./ReferenceCard.css";
+import Box from "@mui/material/Box";
 
 const ReferenceLayout = () => {
   // initial state for skin condition 
   const [condition, setCondition] = useState([]);
   console.log("skincodition", condition);
+
+  const [q, setQ] = useState("");
+  const [searchParam] = useState(["condition_name"]);
+
+  // search function for user be able to search for the condition name
+    const searchItem = (condition) => {
+        return condition.filter((data) => {
+            return searchParam.some((newCondition) => {
+                return (
+                    data[newCondition].toString().toLowerCase().indexOf(q.toLowerCase()) > -1
+                );
+            });
+        });
+    };
+ 
   // fetch skin condition data from backend server
   const getSkinCondition = async () => {
     const response = await fetch("/skinCondition");
@@ -26,22 +42,35 @@ const ReferenceLayout = () => {
       className="app__referencePhoto flex__center section__padding"
       id="reference"
     >
+     
       {/** header title container */}
       <div className="app__referencePhoto">
         <h1>Skin condition on black/brown skin</h1>
+        <div className="search-content">
+                <input
+                    class="input is-success is-rounded"
+                    type="text"
+                    placeholder="Search"
+                    value={q}
+                    onChange={(e) => setQ(e.target.value)}
+                />
+            </div>
         <div className="app__referencePhoto flex__center">
            {/** card container, material grid creates even spacing between the cards 
             * mapping thru database for card layout
            */}
+          
           <div className="app__referencePhoto_items">
-            <Grid container spacing={4}>
-              {condition.map((data, index) => (
-              <Grid item key={data} xs={12} sm={6} md={4}>
+            <Grid container spacing={4} >
+              {searchItem(condition).map((data, index) => (
+                <Grid item key={index} xs={12} sm={6} md={4} >
+                  <Box sx={{ width: 600, height: 450}}>
               <CardInfo key={index}
                 imageUrl={data.photo}
                 title={data.condition_name}
                 details={data.symptoms}
                   />
+                  </Box>
                   </Grid>
             ))}
            </Grid>
