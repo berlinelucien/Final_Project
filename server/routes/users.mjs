7e6e0dc051319f40b69b6bbdea21d9f8487da927
Berlin <Berline_lucien@yahoo.com>
2022-11-09 14:46:32 -0500

    fixing user router post method

--- server/routes/users.mjs
+++ server/routes/users.mjs
@@ -17,23 +17,22 @@ router.get("/", async function (req, res, next) {
 router.post('/users', async (req, res) => {
   const newUser = {
     email: req.body.email,
-    firstName: req.body.given_name,
-    lastName: req.body.family_name,
-    sub: req.body.sub
-
+    name: req.body.name,
+    picture: req.body.picture,
+    sub: req.body.sub,
   }
   console.log(newUser);
 
   const queryEmail = 'SELECT * FROM user_accounts WHERE email=$1 LIMIT 1';
   const valuesEmail = [newUser.email]
   const resultsEmail = await db.query(queryEmail, valuesEmail);
-  if(resultsEmail.rows[0]){
-    console.log(`Thank you ${resultsEmail.rows[0].firstName} for coming back`)
+  if(resultsEmail.rows.length > 0){
+    console.log(`Thank you for coming back`)
   } else{
-  const query = 'INSERT INTO user_accounts(email, firstname, lastname, sub) VALUES($1, $2, $3, $4) RETURNING *'
-  const values = [newUser.email, newUser.firstName, newUser.lastName, newUser.sub]
+    const query = 'INSERT INTO user_accounts(email, name, picture, sub) VALUES($1, $2, $3, $4) RETURNING *';
+  const values = [newUser.email, newUser.name, newUser.picture, newUser.sub]
   const result = await db.query(query, values);
-  console.log(result.rows[0]);
+  console.log(result);
 
   }
 
