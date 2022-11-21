cf88d4da01745ca34a7f624cf208b0cacd571e9e
Berlin <Berline_lucien@yahoo.com>
2022-11-21 18:16:22 -0500

    changed db connection route

--- server/db/db-connection.js
+++ server/db/db-connection.js
@@ -4,10 +4,10 @@ import pgPromise from 'pg-promise';
 // Create Database Connection
 const pgp = pgPromise({});
 
-const db = pgp('postgres://localhost:5432/skinapp');
+const db = pgp(process.env.DATABASE_URL);
 
-//import pg from pg
-//const { Pool } = require('pg');
+// import pg from pg
+// const { Pool } = require('pg');
 // const { Pool } = pg;
 // const db = new Pool({
 //     connectionString: process.env.DATABASE_URL
